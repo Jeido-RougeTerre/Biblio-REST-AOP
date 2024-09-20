0983e8d0f2aa0f12e36015e68994ff24891ade28
Teo Gontard <teo.gontard@gmail.com>
2024-09-20 12:30:53 +0200

    init

---
+++ src/main/java/com/jeido/biblioaop/BiblioAopApplication.java
@@ -0,0 +1,13 @@
+package com.jeido.biblioaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioAopApplication.class, args);
+	}
+
+}
